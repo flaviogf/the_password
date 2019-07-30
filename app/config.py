6d82725248b3b4ac45b404cca6f9d006c6b462f8
flaviogf <flavio.fernandes6@gmail.com>
2019-07-30 10:25:42 -0300

    app.config.Production

--- app/config.py
+++ app/config.py
@@ -10,4 +10,4 @@ class Testing(Config):
 
 
 class Production(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:C894aH86dPAP5V0uuEiV@default.c4wkgwgnxr0p.us-east-1.rds.amazonaws.com/the_password'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/the_password'
